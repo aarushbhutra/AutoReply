d3d2117376a9379cf9b3c305fc7f8a7778ef17d7
Aarush  Bhutra <67967009+aarushbhutra@users.noreply.github.com>
2023-03-04 20:11:46 +0545

    init files

---
+++ src/main/java/me/anon695/autoreply/AutoReply.java
@@ -0,0 +1,17 @@
+package me.anon695.autoreply;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AutoReply extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
